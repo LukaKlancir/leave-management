4ea890babe09200c8f2e46d9d0733ff4fc460aef
Luka Klančir <luka@anada.hr>
2020-05-05 12:03:17 +0200

    Add project files.

---
+++ leave-management/Data/Migrations/20200504111122_AddesEmployeeDataPoints2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace leave_management.Data.Migrations
+{
+    public partial class AddesEmployeeDataPoints2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateCreated",
+                table: "AspNetUsers");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateCreated",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: true);
+        }
+    }
+}
